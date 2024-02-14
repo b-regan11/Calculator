b1a809a5a92f84e9530128796aa4c8aa00f4956e
b-regan11 <reganbill02@gmail.com>
2024-02-14 17:39:42 -0500

    Summation Step

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Calculator {
     
     public static void main(String[] args) {
-        
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter the first whole number you would like to find sum with:");
+        Integer firstNumber = userInput.nextInt();
+        System.out.println("Enter the second whole number you would like to find sum with:");
+        Integer secondNumber = userInput.nextInt();
+        Integer sum;
+        sum = firstNumber + secondNumber;
+        System.out.println("sum = " + sum);
     }
 }
